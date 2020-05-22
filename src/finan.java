a88ddf6867e69a49ba35c104b33e7016eb18dac7
Eduardo Henrique <eduardohenriquemoreirasilva@gmail.com>
2020-05-22 20:54:10 -0300

    Processamento do menu e correção: Leitura do financiamento

--- src/finan.java
+++ src/finan.java
@@ -9,8 +9,8 @@ public class finan {
 		Scanner sc = new Scanner(System.in);
 		
 		String nome;
-		double salario, porcentagem, valorFinanciado, conta;
-		int prestacao;
+		double salario, porcentagem, valorFinanciado, conta, entrada;
+		int prestacao, opcao;
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.println("Nome do cliente: ");
@@ -22,19 +22,56 @@ public class finan {
 			salario = sc.nextDouble();
 		}
 		
-		boolean bol = true;
-		while(bol) {
+		System.out.println("Numero de prestacoes: ");
+		prestacao = sc.nextInt();
+		System.out.println("Porcentagem de entrada: ");
+		porcentagem = sc.nextDouble();
+		System.out.println("Valor total financiado: ");
+		valorFinanciado = sc.nextDouble();
+		conta = ( porcentagem/100.0 )*valorFinanciado;
+		conta = ( valorFinanciado - conta ) / prestacao;
+		while(conta > salario*0.30) {
 			System.out.println("Numero de prestacoes: ");
 			prestacao = sc.nextInt();
 			System.out.println("Porcentagem de entrada: ");
 			porcentagem = sc.nextDouble();
 			System.out.println("Valor total financiado: ");
 			valorFinanciado = sc.nextDouble();
-			
 			conta = ( porcentagem/100.0 )*valorFinanciado;
 			conta = ( valorFinanciado - conta ) / prestacao;
-			if(conta <= salario*0.30) {
-				bol = false;
+		}
+		
+		boolean bol1 = true;
+		while(bol1) {
+			System.out.println(" ");
+			System.out.println("1 - Mostrar valor da entrada");
+			System.out.println("2 - Mostrar o valor financiado");
+			System.out.println("3 - Mostrar valor de cada prestação");
+			System.out.println("4 - Sair");
+			System.out.println("Digite uma opcao: ");
+			opcao = sc.nextInt();
+			
+			if(opcao == 4) {
+				bol1 = false;
+				System.out.println(" ");
+				System.out.println("FIM DO PROGRAMA!");
+			}else if(opcao == 1) {
+				System.out.println(" ");
+				entrada = ( porcentagem/100.0 )*valorFinanciado;
+				System.out.printf("ENTRADA = R$ %.2f%n", entrada);
+				entrada = 0.0;
+			}else if(opcao == 2) {
+				System.out.println(" ");
+				entrada = ( porcentagem/100.0 )*valorFinanciado;
+				entrada =  valorFinanciado - entrada;
+				System.out.printf("VALOR FINANCIADO = R$ %.2f%n", entrada);
+				entrada = 0.0;
+			}else if(opcao == 3) {
+				System.out.println(" ");
+				entrada = ( porcentagem/100.0 )*valorFinanciado;
+				entrada = (valorFinanciado - entrada) / prestacao;
+				System.out.printf("VALOR FINANCIADO = R$ %.2f%n", entrada);
+				entrada = 0.0;
 			}
 		}
 		
